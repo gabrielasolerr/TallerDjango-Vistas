c126bb98ae05cf6c63e997dab1d02bf767a2fcec
gabrielasolerr <g.soler@uniandes.edu.co>
2024-02-10 08:42:02 -0500

    taller3.1

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "taller3",
+        "USER": "gsoler",
+        "PASSWORD": "Jacobo2310",
         "HOST": "localhost",
         "PORT": "",
     }
